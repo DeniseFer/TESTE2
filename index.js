a8cd2e698568cab6545e768421ee11cc54597c6a
Denise Ferreira <124710256+DeniseFer@users.noreply.github.com>
2023-04-04 17:28:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 
+function Alunos(nome,sobrenome,cidade,idade,h){
+    this.name = nome
+    this.sobrename = sobrenome
+    this.city = cidade
+    this.age = idade
+    this.h = ["comer","jogar","ler","pular(de um predio)"]
+    this.nomeCompleto = function(){
+        alert("Seu come completo é "+ this.name +" "+this.sobrename)
+    }
+    
+   console.log("Seus hobbies são: ")
+    for (x in this.h)
+    console.log(this.h[x])
+    
+}
+  let alunos1 = new Alunos("Carla", "de Orleans e Bragança", "Fortaleza", 16)
+  alunos1.nomeCompleto()
+
+
+
+
+
 /* function Triangulo (b,h,a){
     let base = b
     let altura = h
